62e320a60ed956b5a7d6708daae8910982d9b990
namelessone <nlessone@gmail.com>
2019-06-16 15:16:46 +0500

    Fix content type

--- src/main/kotlin/ru/sigil/fantasyradio/backend/controllers/MainController.kt
+++ src/main/kotlin/ru/sigil/fantasyradio/backend/controllers/MainController.kt
@@ -6,9 +6,11 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
@@ -38,7 +40,7 @@ fun Application.main() {
                 val repo by contextedDi.instance<IDbManager>()
                 val gson by contextedDi.instance<Gson>()
                 val last = repo.getLastCurrentStreamInformation() ?: call.respond(HttpStatusCode.NotFound)
-                call.respond(gson.toJson(last))
+                call.respondText(gson.toJson(last), contentType = ContentType.parse("application/json"))
             }
         }
         post("/Crash") {
